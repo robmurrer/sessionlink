70e22a67d56e74fc383cf8384f4a862610c61b0f
Rob Murrer <robmurrer@gmail.com>
2019-09-04 14:24:52 -0700

    de react it

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let titleClick = () => { console.log("huzzah"); };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a className="App-link" href="#" onClick={titleClick}>Session Link</a>
       </header>
+
     </div>
   );
 }
