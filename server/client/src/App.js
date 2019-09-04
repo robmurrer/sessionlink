cfdf9fc0225d12b99286999b591476d3de62b3bf
Rob Murrer <robmurrer@gmail.com>
2019-09-04 15:03:17 -0700

    expressed

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
 
-  let titleClick = () => { console.log("huzzah"); };
+  const titleClick = () => { console.log("huzzah"); };
 
   return (
     <div className="App">
