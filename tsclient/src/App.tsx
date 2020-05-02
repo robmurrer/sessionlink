605c51bed63ff16cbe30af880fa855bf9c364962
ROBERT MURRER <robertmurrer@iMac.localdomain>
2020-05-02 08:42:35 -0700

    re-enable 1.0 as main flow...

--- tsclient/src/App.tsx
+++ tsclient/src/App.tsx
@@ -3,20 +3,48 @@ import logo from './logo.svg';
 import './App.css';
 import { PlayBlock } from './components/PlayBlock';
 import uuid from 'uuid/v4';
-import { Commando } from './Commando';
-import { User } from './User';
+
+//old
+import { BlockProps }  from './components/Block'
+import { Playbox }  from './components/Playbox'
+
+//new
+import { Commando } from './Commando'
+import { User } from './User'
 
 const App: React.FC = () => {
-
   const url = new URL(document.URL);
   let id = url.searchParams.get('id');
+  //http://my.com/?id=test
 
+  //if we have no root_block id on URL, we create one!
   if (id === null) {
-    id = uuid(); 
-    window.location.href = window.location.href + "?id=" + id;
+    id = uuid() 
+    window.location.href = window.location.href + "?id=" + id
   }
 
+  let b0: BlockProps = {
+    id: id,
+    title: "hi jay",
+    value: "test"
+  }
+
+  return (
+    <div className="App">
+      <Playbox block={b0}></Playbox>
+    </div>
+  )
+}
+/*
+  // Hierarchical Block Flow (not working w/ Sync)
+  //
+  //Commando is our command processor
+  // it will sync to local storage and send to server
+  //
   const commando = new Commando(id)
+
+  //Sees if user has been created before,
+  //if not then it will create
   const user = User.Create(); 
 
   return (
@@ -25,5 +53,6 @@ const App: React.FC = () => {
     </div>
   );
 }
+*/
 
 export default App;
