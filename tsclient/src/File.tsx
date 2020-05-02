b2f41dcf09af2d80c2054fd392c3dcb9c85cc565
Rob Murrer <robmurrer@gmail.com>
2020-05-02 15:06:43 -0700

    added some image compression for filedrop v1
    hope to save some cycles

--- tsclient/src/File.tsx
+++ tsclient/src/File.tsx
@@ -7,3 +7,64 @@ export function getBase64(file: File) {
       reader.onerror = error => reject(error);
     });
 }
+
+//copy pasta from: https://stackoverflow.com/questions/14672746/how-to-compress-an-image-via-javascript-in-the-browser
+function getImage(dataUrl: string): Promise<HTMLImageElement> 
+{
+    return new Promise((resolve, reject) => {
+        const image = new Image();
+        image.src = dataUrl;
+        image.onload = () => {
+            resolve(image);
+        };
+        image.onerror = (el: any, err: any) => {
+            reject(err.error);
+        };
+    });
+}
+
+//copy pasta from: https://stackoverflow.com/questions/14672746/how-to-compress-an-image-via-javascript-in-the-browser
+export async function downscaleImage(
+        dataUrl: string,  
+        imageType: string,  // e.g. 'image/jpeg'
+        resolution: number,  // max width/height in pixels
+        quality: number   // e.g. 0.9 = 90% quality
+    ): Promise<string> {
+
+    // Create a temporary image so that we can compute the height of the image.
+    const image = await getImage(dataUrl);
+    const oldWidth = image.naturalWidth;
+    const oldHeight = image.naturalHeight;
+    console.log('dims', oldWidth, oldHeight);
+
+    const longestDimension = oldWidth > oldHeight ? 'width' : 'height';
+    const currentRes = longestDimension == 'width' ? oldWidth : oldHeight;
+    console.log('longest dim', longestDimension, currentRes);
+
+    if (currentRes > resolution) {
+        console.log('need to resize...');
+
+        // Calculate new dimensions
+        const newSize = longestDimension == 'width'
+            ? Math.floor(oldHeight / oldWidth * resolution)
+            : Math.floor(oldWidth / oldHeight * resolution);
+        const newWidth = longestDimension == 'width' ? resolution : newSize;
+        const newHeight = longestDimension == 'height' ? resolution : newSize;
+        console.log('new width / height', newWidth, newHeight);
+
+        // Create a temporary canvas to draw the downscaled image on.
+        const canvas = document.createElement('canvas');
+        canvas.width = newWidth;
+        canvas.height = newHeight;
+
+        // Draw the downscaled image on the canvas and return the new data URL.
+        const ctx = canvas.getContext('2d')!;
+        ctx.drawImage(image, 0, 0, newWidth, newHeight);
+        const newDataUrl = canvas.toDataURL(imageType, quality);
+        return newDataUrl;
+    }
+    else {
+        return dataUrl;
+    }
+
+}
--- tsclient/src/components/FileDrop.tsx
+++ tsclient/src/components/FileDrop.tsx
@@ -1,58 +1,79 @@
-import * as React from "react";
-import { BlockProps, BlockTypes } from "./Block";
-import uuid from "uuid";
-import { Command } from "./Playbox";
+import * as React from "react"
+import { BlockProps, BlockTypes } from "./Block"
+import uuid from "uuid"
+import { Command } from "./Playbox"
+
+import { getBase64, downscaleImage } from '../File'
 
 export interface FileDropProps {
  commando: Function,
 }
 
-
+//todo extract the breakpoints for scaling and sizing
 export class FileDrop extends React.Component<FileDropProps> {
-
     constructor(props: FileDropProps) {
-        super(props);
-    }
-    getBase64(file: File) {
-        return new Promise((resolve, reject) => {
-          const reader = new FileReader();
-          reader.readAsDataURL(file);
-          reader.onload = () => resolve(reader.result);
-          reader.onerror = error => reject(error);
-        });
+        super(props)
     }
 
-    dropHandler(event: React.DragEvent) {
-        event.preventDefault();
-        // console.log("dragHandler");
-        if (!event.dataTransfer.items) return;
+    async dropHandler(event: React.DragEvent) {
+        event.preventDefault()
+        if (!event.dataTransfer.items) return
 
         for (let i=0; i<event.dataTransfer.items.length; i++) {
-            let item = event.dataTransfer.items[i];
-            //console.log(item);
-            if (item.kind !== "file") continue;
+            let item = event.dataTransfer.items[i]
+            if (item.kind !== "file") continue
 
-            let f = item.getAsFile();
-            if (!f) return;
+            let f = item.getAsFile()
+            if (!f) return
 
-            this.getBase64(f).then(data => { 
-                if (!f) return;
+            let stomp = false
+            if (f.size > 100*1024) { //100 kilobytes?
+                console.log('$ Stomping on a file w/ size: ', f.size / 1e6, " ~ megabytes")
+                if (f.type.startsWith("image")) {
+                    stomp = true
+                } //todo more ways of stomping (tail/head of txt etc)
+                else {
+                    console.log('$ Impossible stomp :( send <3 and $ to cryptide.com')
+                    return
+                }
+            }
+
+            getBase64(f).then(data => { 
+                if (stomp) {
+                    downscaleImage(data as string, "image/png", 600, .99).then(data => {
+                        if (!f) return //typescript :P
+                        console.log('$ New size ', (new Blob([data])).size / 1e6, " ~ megabytes")
+
+                        let new_block: BlockProps = {
+                            id: uuid(),
+                            title: f.name,
+                            type: BlockTypes.FILE,
+                            value: data 
+                        }
+
+                        this.props.commando(new_block, Command.Create)
+                    })
+
+                    return
+                }
+
+                //repeats :(
+                if (!f) return
                 let new_block: BlockProps = {
                     id: uuid(),
                     title: f.name,
                     type: BlockTypes.FILE,
-                    value: data
+                    value: data 
                 }
 
-                this.props.commando(new_block, Command.Create);
-            });
+                this.props.commando(new_block, Command.Create)
+            })
         }
-
     }
 
     dragOverHandler(event: React.DragEvent) {
         //console.log("dragOver");
-        event.preventDefault();
+        event.preventDefault()
     }
 
     render() {
